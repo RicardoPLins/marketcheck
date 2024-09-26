ac80554eeea18fe4bb0c010f10d709579e9c9745
RicardoPLins <ricardinho200215@gmail.com>
2024-09-26 00:01:26 -0300

    fix - corrigido o redirecionamento após o login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery unless: -> { Rails.env.development? }
   before_action :authenticate_user!
   def after_sign_in_path_for(resource)
         stored_location_for(resource) || produtos_path
