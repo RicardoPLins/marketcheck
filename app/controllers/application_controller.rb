46a9cd503971e0dd40edaa3c07935f6f85ccde62
Walter <walterfilho1000@gmail.com>
2024-09-29 22:22:11 -0300

    ajuste feito

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery unless: -> { Rails.env.development? }
   
   # Permitir a ação de login
-  before_action :authenticate_user!, except: [:add] # Modifique 'create' se necessário
-
+  before_action :authenticate_user!
+  
   rescue_from CanCan::AccessDenied do |exception|
     render json: { error: exception.message }, status: :forbidden
   end
