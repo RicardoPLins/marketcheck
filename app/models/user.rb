8d0fadc88c44e89f94848c63eb1aa20021691862
RicardoPLins <ricardinho200215@gmail.com>
2024-09-30 23:20:25 -0300

    add - views do users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,22 @@
 class User < ApplicationRecord
   has_one :carrinho
-  after_create :create__empty_cart
-  enum role: {user: 0, admin:1}
+  after_create :create_empty_cart
+  enum role: { user: 0, admin: 1 }
+
+  before_create :assign_role
 
   private 
-  def create__empty_cart
+
+  def create_empty_cart
     Carrinho.create(user_id: self.id)
   end
+
+  # Atribui a role baseado no checkbox
+  def assign_role
+    self.role ||= :user # Define como user por padrão
+    self.role = :admin if self.role == '1' # Se o checkbox foi marcado
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
